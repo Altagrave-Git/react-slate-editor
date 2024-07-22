527a69ed8fb79d6ca14aed92152eec2b82c3a157
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-07-22 03:39:26 -0600

    Add initial setup for slate editor in main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-function App() {
-  return null;
+import { useState } from 'react';
+import { createEditor, BaseEditor, Descendant } from 'slate';
+import { Slate, Editable, withReact, ReactEditor } from 'slate-react';
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] };
+type CustomText = { text: string; bold?: true };
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+};
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'Example text in a paragraph node.' }],
+  },
+];
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()));
+
+  return (
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable />
+    </Slate>
+  )
 };
 
 export default App;
