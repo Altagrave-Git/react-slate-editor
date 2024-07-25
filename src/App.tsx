2e5cad30c3091b43f3605cd8f70e16dd0cc936ce
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-07-25 08:07:34 -0600

    Add support for code, paragraph elements, and keyboard shortcuts in editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import { createEditor, BaseEditor, Descendant, Transforms, Editor, Element } from 'slate';
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react';
 
-type CustomElement = { type: string; children: CustomText[] };
+type CustomElement = { type: string; children: CustomElement[] | CustomText[] };
 type CustomText = { text: string };
 
 declare module 'slate' {
@@ -32,26 +32,38 @@ const App = () => {
     }
   }, []);
 
+  const renderLeaf = useCallback((props: any) => {
+    return <Leaf {...props} />
+  }, []);
+
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === '&') {
       e.preventDefault();
       editor.insertText('and');
     }
-    if (e.key === '`' && e.ctrlKey) {
-      e.preventDefault();
-
-      // Determine whether any of the currently selected blocks are code blocks
-      const [match] = Editor.nodes(editor, {
-        match: n => Element.isElement(n) && n.type === 'code',
-      })
-
-      // Toggle the block type depending on whether there's already a match.
-      Transforms.setNodes(
-        editor,
-        { type: match ? 'paragraph' : 'code' },
-        { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
-      )
-
+    if (e.key && e.ctrlKey) {
+      switch (e.key) {
+        case '`': {
+          e.preventDefault();
+          // Determine whether any of the currently selected blocks are code blocks
+          const [match] = Editor.nodes(editor, {
+            match: n => Element.isElement(n) && n.type === 'code',
+          })
+          // Toggle the block type depending on whether there's already a match.
+          Transforms.setNodes(
+            editor,
+            { type: match ? 'paragraph' : 'code' },
+            { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
+          )
+          break;
+        }
+  
+        case 'b': {
+          e.preventDefault();
+          Editor.addMark(editor, 'bold', true);
+          break;
+        }
+      }
     }
   };
 
@@ -61,6 +73,8 @@ const App = () => {
         placeholder='Enter some text...'
         onKeyDown={handleKeyDown}
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        spellCheck={false}
       />
     </Slate>
   )
@@ -76,6 +90,20 @@ const CodeElement = (props: any) => {
       <code>{props.children}</code>
     </pre>
   )
-}
+};
+
+const Leaf = ({attributes, children, leaf}: any) => {
+  return (
+    <span
+      {...attributes}
+      style={{
+        fontWeight: leaf.bold ? '700' : '400',
+        fontStyle: leaf.italic ? 'italic' : 'normal',
+      }}
+    >
+      {children}
+    </span>
+  )
+};
 
 export default App;
